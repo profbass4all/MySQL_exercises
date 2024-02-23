8db5c5097d356b9554b13ebbfe90e8ab63ad9db6
Abass Ibrahim <profbass4all@gmail.com>
2024-02-23 16:22:36 +0100

    The answers to worksheet one has been added

---
+++ Readme.md
@@ -0,0 +1,21 @@
+This folder contains about 150 questions that test your basic knowledge of MySql.
+
+The solutions are also available in the solutions folder and in their respective file for each worksheet.
+
+The problems covers the following areas of MySql:
+
+1. Database Related Commands
+
+2. Table Related Commands
+
+3. Insert Into Commands
+
+4. Update and Delete Commands
+
+5. Select Commands
+
+6. Single Row Functions
+
+7. Aggregate Functions
+
+8. Joins
---
+++ solutions/worksheet1.sql
@@ -0,0 +1,51 @@
+-- question 1 If a database "Employee" exists, which MySql command helps you to start working in that database? 
+-- Answer 
+use employee;
+
+-- question 2 Write MySql command will be used to open an already existing database "LIBRARY".
+-- Answer
+use library;
+
+-- question 3 Write MySql command to open an existing database.
+-- Answer
+use database1;
+
+-- question 4 What does SQL stand for? What is MySQL?
+-- Answer 
+Structured Query Language... MySQL is a relational database management software
+
+-- question 5 Write two examples of DBMS software.
+-- Answer
+MongoDb and MySQL
+
+-- question 6 Sherifat wants to make the database named ‘COMPANY’ active. Write MySQL commands for it.
+-- Answer
+use company;
+
+-- question 7 What is MySQL
+-- Answer
+MySQL is a relational database management software
+
+-- question 8 What is the relationship between SQL and MySQL 
+-- Answer
+SQL is the programming language used to query data stored in a database while MySql is a database management software.
+
+-- question 9 Mention any two example of common Database Management System.
+-- Answer
+MySQL and Oracle
+
+-- question 10 suggest sql query suitable command for the following purpose:
+-- (i)To display the list of the database already existing in MySQL.
+show databases;
+-- (ii) To use the database named City.
+use city;
+-- (iii) To remove the pre-existing database named Clients
+drop database clients;
+
+-- question 11 write a command to display the name of the active database
+-- Answer
+Select DATABASE()
+
+-- question 12 Write the command to create a new database “School”
+-- Answer
+create database school;
