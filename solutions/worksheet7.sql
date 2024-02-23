df41833b343c08355cd47117d3f77cde638b7349
Abass Ibrahim <profbass4all@gmail.com>
2024-02-23 22:35:56 +0100

    I have just added the worksheet7

---
+++ solutions/worksheet7.sql
@@ -0,0 +1,29 @@
+--1
+a) 6.57
+b) 5.3
+c) 25
+d) lass 12
+
+-- 2 
+i) 4
+ii)ics 
+
+-- 3
+now() shows the present date and time while sysdate() depends on the date and time of the system. 
+
+--4
+trim()
+
+-- 5
+unclear table 
+
+-- 6
+unclear table 
+
+-- 7
+i) 6
+ii) 10
+iii) 600
+iv) 30
+
+-- 8
